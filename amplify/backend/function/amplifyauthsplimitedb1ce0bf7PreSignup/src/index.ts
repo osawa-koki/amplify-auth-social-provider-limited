775e6ef4719e98d9120a9fe40f52d02829ce76f7
osawa-koki <koki@osawa.dev>
2024-04-27 18:12:24 +0900

    setup

--- amplify/backend/function/amplifyauthsplimitedb1ce0bf7PreSignup/src/index.ts
+++ amplify/backend/function/amplifyauthsplimitedb1ce0bf7PreSignup/src/index.ts
@@ -5,15 +5,17 @@
  * specified in the `MODULES` env var, located at `./${MODULE}`.
  */
 
+import { APIGatewayProxyEvent, APIGatewayEventRequestContext } from 'aws-lambda'
+
 /**
  * The names of modules to load are stored as a comma-delimited string in the
  * `MODULES` env var.
  */
-const moduleNames = process.env.MODULES.split(',');
+const moduleNames = (process.env.MODULES ?? '').split(',')
 /**
  * The array of imported modules.
  */
-const modules = moduleNames.map((name) => require(`./${name}`));
+const modules = moduleNames.map((name) => require(`./${name}`))
 
 /**
  * This async handler iterates over the given modules and awaits them.
@@ -22,12 +24,12 @@ const modules = moduleNames.map((name) => require(`./${name}`));
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  *
  */
-exports.handler = async (event, context) => {
+exports.handler = async (event: APIGatewayProxyEvent, context: APIGatewayEventRequestContext): Promise<APIGatewayProxyEvent> => {
   /**
    * Instead of naively iterating over all handlers, run them concurrently with
    * `await Promise.all(...)`. This would otherwise just be determined by the
    * order of names in the `MODULES` var.
    */
-  await Promise.all(modules.map((module) => module.handler(event, context)));
-  return event;
-};
+  await Promise.all(modules.map((module) => module.handler(event, context)))
+  return event
+}
