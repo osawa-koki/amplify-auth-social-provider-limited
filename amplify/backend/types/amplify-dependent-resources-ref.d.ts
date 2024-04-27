f20093ff5262635edfc9020a0605823c8453de23
osawa-koki <koki@osawa.dev>
2024-04-27 17:29:01 +0900

    small fix

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,17 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "amplifyauthsplimited7632fb71": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "function": {
     "amplifyauthsplimited7632fb71PreSignup": {
       "Arn": "string",
